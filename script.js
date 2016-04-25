6641999511db3669eab5a30f4ae3e9e7568eb003
isolanilla <i.solanilla3@gmail.com>
2016-04-25 14:01:12 +0200

    A

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
 
-  $(function() {
-    $( "#tabs" ).tabs();
-  });
-  $("#mytab").click(function(){
-    $.getJSON("myline.json", function(data) {
-        
-  	});
+    $("#tabs").tabs();
 
-  })
+  $.getJSON("timeline.json", function(data) {
+      timeline="";
+      for (var i = 0; i < data.length; i++) {
+        console.log("BUCLE")
+        timeline += "<h3> Autor:" + data[i].autor 
+        timeline = timeline + "<p class='imagen'>"+  data[i].avatar+ "</p>"
+        timeline = timeline + "<p>" + data[i].fecha + "</p><h3>"
+        timeline = timeline + "<div><p> titulo:" + data[i].titulo + "</p>"
+        timeline = timeline + "<p> contenido "+data[i].contenido +"</p></div>"
+
+      }
+      console.log(timeline)
+      $("#accordion").append(timeline);
+      $("#accordion").accordion();
+      $("#accordion2").accordion();
 
 
+
+	});
 
 });
